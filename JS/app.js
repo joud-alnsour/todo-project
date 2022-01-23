8ad98c38d00b4543ce43f2692ad086daab343426
joud-alnsour <alnsour.joud@gmail.com>
2022-01-23 23:05:41 +0200

    add js2

--- JS/app.js
+++ JS/app.js
@@ -1,20 +1,56 @@
 'use strict';
 var Name = window.prompt("Enter your name: ");
 
-var gender = window.prompt("Enter your gender (male or female) : ");
+var gender = window.prompt("Enter your gender (male or female) : ").toLowerCase();
 
-var Age = window.prompt("Enter your age  (>=0) : ");
+var age = window.prompt("Enter your age: ");
+if (age <= 0) {
+    alert('The age is less than or equal to zero.')
+    age = prompt('enter the age')
+}
+var welcome = window.prompt("if you wants to skip the welcoming message write yes");
 
-var welcome  = window.prompt("if you wants to skip the welcoming message write yes");
- 
- if (gender=='male'&&welcome!='yes')
-{var x='Hello Mr '+ Name + '..';
-alert(x)
+var Hi = window.prompt("do you read a book in the week ?");
 
-}else if (gender=='female'&&welcome!='yes'){
-    var x='Hello Ms '+ Name + '..';
-    alert(x)
-}else if (welcome=='yes'){
-    prompt('Enter you email:')
-}else
-alert('Welcome our website');
+var Person = window.prompt("do you have a job ?");
+
+
+var People = window.prompt("do you love animals ?");
+
+if (welcome != 'yes')
+ { if(gender!='male'&&gender!='female')
+ {
+     alert('gender is invalid')
+     alert('hello '+Name)
+ }
+    else if (gender == 'male') {
+        alert('Hello Mr ' + Name + '..')
+    } else if (gender == 'female') {
+   
+    alert('Hello Ms ' + Name + '..')
+}
+
+}
+
+function task (ask)
+{
+let reply =[];
+for (let index = 0;index < ask.length; index++)
+{
+    let firstreply =prompt(ask[index]).toLowerCase();
+    if (firstreply ==='')
+    {
+        reply.push('invalid');}
+        else if(firstreply === 'yes' || firstreply === 'no')
+        {
+          reply.push(firstreply);
+        }
+        else{
+          alert('invalid');
+        }
+    }
+        console.log(reply);
+        return reply;
+}
+
+task (['do you read a book in the week ?','do you have a job ?','do you love animals ?']);
